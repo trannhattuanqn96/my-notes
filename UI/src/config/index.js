b6736b86ca7d9fc81d85a21dccb8710e8c3d86b1
Tuan Tran <trannhattuanqn96@gmail.com>
2024-03-10 20:37:42 +0700

    update config

--- UI/src/config/index.js
+++ UI/src/config/index.js
@@ -7,7 +7,7 @@ if (process.env.ENVIRONMENT === 'dev') {
     }
 } else {
     dataConfig = {
-        baseURLAPI: 'http://mynote.tuandevzz77.io.vn/api'
+        baseURLAPI: 'http://mynote.tuandevzz77.io.vn'
     }
 }
 
