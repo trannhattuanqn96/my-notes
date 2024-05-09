1b310b073d49bc7aaa57b27da3d0611bdbc55390
= <trannhattuanqn96@gmail.com>
2024-05-09 11:20:53 +0700

    update

--- API/src/routers/index.js
+++ API/src/routers/index.js
@@ -7,7 +7,7 @@ import {
 } from "../controller/Notes.controller.js";
 const useRoutes = (app) => {
     app.get("/", (req, res) => {
-        res.send("Welcome to the Node.js Tutorial! - test ");
+        res.send("Welcome to the Node.js Tutorial! - tuantran ");
     });
 
     app.use(`/login`, loginRouter);
