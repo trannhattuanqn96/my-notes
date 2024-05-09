b6a6cedd7db1dadf3dcd56afc711d210ce0b775e
Tuan Tran <trannhattuanqn96@gmail.com>
2024-05-10 00:48:30 +0700

    a

--- API/src/app.js
+++ API/src/app.js
@@ -12,30 +12,11 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
-// enable CORS
-// var corsOptions = ["http://notes.tuandevzz77.site/", "http://localhost:3002"];
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     // bypass the requests with no origin (like curl requests, mobile apps, etc )
-//     if (!origin) return callback(null, true);
 
-//     if (corsOptions.indexOf(origin) === -1) {
-//       var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
-
-// const corsOptions = {
-//   origin: 'http://notes.tuandevzz77.site/', // Chỉ cho phép truy cập từ domain này
-// };
-const corsOptions = {
-  origin: '*', // Chỉ cho phép truy cập từ domain này
-};
-
+var corsOptions = {
+  origin: "http://notes.tuandevzz77.site/",
+}
 app.use(cors(corsOptions));
-// app.use(cors(corsOptions));
 // parse application/json
 app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
