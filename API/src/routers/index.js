d674c7b643b08ddfdd8dc8744193149856ef76c6
= <trannhattuanqn96@gmail.com>
2024-05-13 16:21:30 +0700

    update2

--- API/src/routers/index.js
+++ API/src/routers/index.js
@@ -7,7 +7,7 @@ import {
 } from "../controller/Notes.controller.js";
 const useRoutes = (app) => {
     app.get("/", (req, res) => {
-        res.send("Welcome to the tuantrandevzz ");
+        res.send("Welcome to the tuantrandevzz77 ");
     });
 
     app.use(`/login`, loginRouter);
